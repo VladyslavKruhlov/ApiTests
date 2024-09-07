f795c30fee86789557135ecf5b01d43558dd642c
VladyslavKruhlov <mr.kruglov1994@gmail.com>
2024-09-07 22:18:46 +0300

    change directory for Constant. Put all this content to test/java/api directory

--- src/main/java/pojo/Constants.java
+++
@@ -1,9 +0,0 @@
-package pojo;
-
-public class Constants {
-    public static final String JIRA_URL = "https://jira.ithillel.com";
-    public static final String JIRA_ENDPOINT_FOR_CREATE_ISSUE = "/rest/api/2/issue/";
-    public static final String JIRA_KEY = "U3QAJ201023";
-    public static final String JIRA_USERNAME = "mr.kruglov1994";
-    public static final String JIRA_PASSWORD = "01021994";
-}
